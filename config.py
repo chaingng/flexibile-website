2add1568c76a874ef3af32b08d3126a2aee4090d
chaingng <chngng0103@gmail.com>
2016-02-18 10:26:06 +0900

    footerを編集

--- config.py
+++ config.py
@@ -1,2 +1,9 @@
 # -*- coding: utf-8 -*-
-TITLE="my-title"
+
+#title
+title="my-title"
+
+#map
+maplat="35.7105593"
+maplon="139.8107326"
+map_info=u"東京都墨田区"
--- main.py
+++ main.py
@@ -5,13 +5,15 @@ import config
 env = Environment(loader=FileSystemLoader('./html/', encoding='utf8'))
 t = env.get_template('index.html')
 
-#foods = []
-#foods.append({'name':u'ラーメン', 'price':400})
-#foods.append({'name':u'焼き飯',   'price':500})
-#foods.append({'name':u'天津飯',   'price':600})
-
 html = t.render({
-    'title' : config.TITLE
+    'title'  : config.title,
+    'maplat' : config.maplat,
+    'maplon' : config.maplon,
+    'map_info' : config.map_info,
+    'footer_on' : False
          })
 #print 'Content-Type: text/html; charset=utf-8\n'
-print html.encode('utf-8')
+
+f = open('html/test.html','w')
+f.write(html.encode('utf-8'))
+f.close()
