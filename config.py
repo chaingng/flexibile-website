248f2080e729ff8c54d32d5b96a68e0148c2f659
chaingng <chngng0103@gmail.com>
2016-03-08 23:11:25 +0900

    remove unnecessary pages

--- config.py
+++ config.py
@@ -1,71 +1,89 @@
 # -*- coding: utf-8 -*-
 
+appeal_on = True
+smartphone_on = False
+contact_on = True
+counter_on = False
+review_on = False
+shots_on = False
+buyitnow_on = False
+skills_on = False
+
 #title
-title="my-title"
+title=u"春名工務店"
 
 #map
-maplat="35.7105593"
-maplon="139.8107326"
-map_info=u"東京都墨田区"
+map_on = True
+map = {
+ 'maplat' : "34.6629349",
+ 'maplon' : '135.0182208',
+ 'info' : u"兵庫県神戸市西区伊川谷町有瀬696−47 セジュール有瀬"
+}
 
 #top
 top = []
 top.append({
-    'title' : u"Beetle",
-    'detail': u"安心と快適",
-    'img' : 'img/custom/top.jpg'
+    'title' : u"完全自由設計",
+    'detail': u"家族がくつろげる癒やしの空間。",
+    'img' : 'img/custom/top3.jpg'
 })
 top.append({
-    'title' : u"リフォーム2",
+    'title' : u"リフォーム",
     'detail': u"お客様のライフスタイルに合わせた住まいを提案致します。",
-    'img' : 'img/custom/1.jpg'
+    'img' : 'img/custom/top4.jpg'
 })
 
 #summary
+summary_on = True
 summary = {
     'title1' : u"リフォーム",
     'detail1': u"お客様のライフスタイルに合わせた住まいを提案致します。",
-    'title2' : u"安心の耐震性",
+    'title2' : u"エコロジカルで長寿命な住まいづくり",
     'detail2': u"住宅の振動実験を行い強度を検証しています。",
-    'title3' : u"価格",
-    'detail3': u"お客様の予算内でのリーズナブルな価格でご提供します。"
+    'title3' : u"どこよりも安く",
+    'detail3': u"ハウスメーカー並のハイグレードな仕様でありながら、建てる時も建てた後の費用も圧倒的に安い家を実現します。"
 }
 
 #portfolio
+portfolio_on = True
 portfolio = []
 portfolio.append({
-    'title' : u"リフォーム1",
-    'detail': u"お客様のライフスタイルに合わせた住まいを提案致します。",
-    'img' : 'img/custom/3.jpg'
+    'title' : u"岐阜県大垣市内",
+    'detail': u"平成２３年度　東海環状自動車道大垣地区整備工事",
+    'img' : 'img/custom/10.jpg'
 })
 portfolio.append({
-    'title' : u"リフォーム2",
-    'detail': u"お客様のライフスタイルに合わせた住まいを提案致します。",
-    'img' : 'img/custom/2.jpg'
+    'title' : u"岐阜県揖斐郡池田町内",
+    'detail': u"第通砂5-2号　公共社会資本整備総合交付金（通常砂防事業）",
+    'img' : 'img/custom/11.jpg'
 })
 portfolio.append({
-    'title' : u"リフォーム3",
-    'detail': u"お客様のライフスタイルに合わせた住まいを提案致します。",
-    'img' : 'img/custom/3.jpg'
+    'title' : u"神戸市、K様邸",
+    'detail': u"広々ゆったり空間で開放的な上質の家",
+    'img' : 'img/custom/8.jpg'
 })
 portfolio.append({
-    'title' : u"リフォーム4",
-    'detail': u"お客様のライフスタイルに合わせた住まいを提案致します。",
-    'img' : 'img/custom/4.jpg'
+    'title' : u"大阪市福島区 Ｔ様邸",
+    'detail': u"シンプルかつ収納の多さが魅力の家",
+    'img' : 'img/custom/9.jpg'
 })
 portfolio.append({
-    'title' : u"リフォーム5",
+    'title' : u"三田市 Ｓ様邸",
     'detail': u"お客様のライフスタイルに合わせた住まいを提案致します。",
-    'img' : 'img/custom/2.jpg'
+    'img' : 'img/custom/9.jpg'
 })
 portfolio.append({
-    'title' : u"リフォーム6",
-    'detail': u"お客様のライフスタイルに合わせた住まいを提案致します。",
-    'img' : 'img/custom/6.jpg'
+    'title' : u"加古川市、H様邸",
+    'detail': u"快適な住み心地を考えた全面バリアフリーの家",
+    'img' : 'img/custom/8.jpg'
 })
 
 
 #contact
-contact_company=u"スカイツリー"
-contact_location=u"〒131-0045 東京都墨田区押上１丁目１−２"
+contact_company=u"株式会社春名工務店"
+contact_location=u"〒651-2113 兵庫県神戸市西区伊川谷町有瀬696−47 セジュール有瀬"
 contact_comment=u"住まい全般についてのご質問やご相談など、お電話とフォームにてお問い合わせいただけます。"
+contact_tel=u"078-978-0570"
+
+#copyright
+copyright = u"Haruna Koumu Ten."
--- main.py
+++ main.py
@@ -3,32 +3,34 @@ from jinja2 import Environment, FileSystemLoader
 import config
 
 env = Environment(loader=FileSystemLoader('./html/', encoding='utf8'))
-t = env.get_template('index.html')
+t = env.get_template('base.html')
 
 html = t.render({
     'title'  : config.title,
-    'maplat' : config.maplat,
-    'maplon' : config.maplon,
-    'map_info' : config.map_info,
-    'smartphone_on' : False,
     'summary' : config.summary,
     'portfolio' : config.portfolio,
     'top' : config.top,
-    'contact_on' : True,
+    'map' : config.map,
     'contact_location' : config.contact_location,
     'contact_company' : config.contact_company,
-    'contact_comment' : config.contact_comment  ,
-    'counter_on' : False,
-    'summary_on' : True,
-    'review_on' : False,
-    'portfolio_on' : True,
-    'shots_on' : False,
-    'buyitnow_on' : False,
-    'map_on' : True,
-    'skills_on' : False
-         })
+    'contact_comment' : config.contact_comment,
+    'contact_tel' : config.contact_tel,
+    'copyright' : config.copyright,
+    'appeal_on' : config.appeal_on,
+    'smartphone_on' : config.smartphone_on,
+    'contact_on' : config.contact_on,
+    'counter_on' : config.counter_on,
+    'summary_on' : config.summary_on,
+    'review_on' : config.review_on,
+    'portfolio_on' : config.portfolio_on,
+    'shots_on' : config.shots_on,
+    'buyitnow_on' : config.buyitnow_on,
+    'map_on' : config.map_on,
+    'skills_on' : config.skills_on
+})
+
 #print 'Content-Type: text/html; charset=utf-8\n'
 
-f = open('html/test.html','w')
+f = open('html/index.html','w')
 f.write(html.encode('utf-8'))
 f.close()
